ccbb06757ff682cd004dd34b4f20edc3a18eda3d
Hristo Georgiev <6043312+hristog@users.noreply.github.com>
2024-02-24 19:57:27 +0000

    Temp change: comment (examples/gui/platform/build.rs)

--- examples/gui/platform/build.rs
+++ examples/gui/platform/build.rs
@@ -1,3 +1,4 @@
+// This is a comment!
 fn main() {
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=dylib=app");
@@ -6,4 +7,11 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=libapp");
 
     println!("cargo:rustc-link-search=.");
+
+    // A commented-out line.
+    // println!("cargo:rustc-link-search=.");
 }
+
+
+// More commented-out stuff:
+// fn main() {}
