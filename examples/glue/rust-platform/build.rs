175fb119f7c6d5362c355a478edc211dd11a8de6
Hristo Georgiev <6043312+hristog@users.noreply.github.com>
2024-02-24 21:01:47 +0000

    Multiple non-comment changes to rs files

--- examples/glue/rust-platform/build.rs
+++ examples/glue/rust-platform/build.rs
@@ -1,9 +1,11 @@
+#[cfg(not(windows))]
+#[cfg(not(linux))]
 fn main() {
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=dylib=app");
 
     #[cfg(windows)]
     println!("cargo:rustc-link-lib=dylib=libapp");
-
+    #[cfg(not(windows))]
     println!("cargo:rustc-link-search=.");
 }
