1dc4cfe752d6d7274b79fb6d052033776f6e7e59
Hristo Georgiev <6043312+hristog@users.noreply.github.com>
2024-02-24 20:54:20 +0000

    Multiple rs comment changes

--- examples/gui/platform/build.rs
+++ examples/gui/platform/build.rs
@@ -1,9 +1,13 @@
+   // Test change!
 fn main() {
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=dylib=app");
 
+//
     #[cfg(windows)]
     println!("cargo:rustc-link-lib=dylib=libapp");
 
     println!("cargo:rustc-link-search=.");
 }
+
+// Test change!
