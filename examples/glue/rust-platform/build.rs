f6a912ce68ef7422562bbbaefca2fab001e7f537
Hristo Georgiev <6043312+hristog@users.noreply.github.com>
2024-02-25 09:41:23 +0000

    rs comment change (examples/glue/rust-platform/build.rs)

--- examples/glue/rust-platform/build.rs
+++ examples/glue/rust-platform/build.rs
@@ -1,9 +1,25 @@
+                // TEST
 fn main() {
     #[cfg(not(windows))]
     println!("cargo:rustc-link-lib=dylib=app");
-
+           /* Test comment here *//
     #[cfg(windows)]
     println!("cargo:rustc-link-lib=dylib=libapp");
 
     println!("cargo:rustc-link-search=.");
 }
+
+
+
+
+
+
+
+
+
+
+                        // Another test comment over here.
+
+
+
+               
