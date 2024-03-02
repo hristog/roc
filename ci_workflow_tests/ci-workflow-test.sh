128da9e1aa394dfd09a63193e82228c256813925
Hristo Georgiev <6043312+hristog@users.noreply.github.com>
2024-03-02 21:13:03 +0000

    Update `update-all-tc-branches`

--- ci_workflow_tests/ci-workflow-test.sh
+++ ci_workflow_tests/ci-workflow-test.sh
@@ -59,13 +59,17 @@ run-rs-check() {
 }
 
 update-all-tc-branches() {
+    original_dir=$(pwd)
     original_branch=$(git branch | grep \* | cut -d' ' -f2)
+
+    cd ..
     for test_case in $(cat ./test_cases.expected); do
         branch_name=$(echo ${test_case} | cut -d, -f1)
         git checkout ${branch_name}
         git rebase main
     done
     git checkout ${original_branch}
+    cd ${original_dir}
 }
 
 "$@"
